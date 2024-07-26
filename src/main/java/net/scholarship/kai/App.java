cf815551e5e4d0cc75a450fc72519022f33b49cf
MilkyBruva <kai.mcenroe2@gmail.com>
2024-07-26 12:45:27 +1200

    [a1] added fontawesome icons and get a basic window setup

--- src/main/java/net/scholarship/kai/App.java
+++ src/main/java/net/scholarship/kai/App.java
@@ -3,8 +3,13 @@ package net.scholarship.kai;
 import imgui.ImGui;
 import imgui.app.Application;
 import imgui.app.Configuration;
+import imgui.flag.ImGuiWindowFlags;
+import imgui.type.ImBoolean;
 
 public class App extends Application {
+
+    public ImBoolean checked = new ImBoolean(false);
+
     public static void main( String[] args ) {
         
         launch(new App());
@@ -13,15 +18,20 @@ public class App extends Application {
 
     @Override
     protected void configure(Configuration config) {
-
+        
         config.setTitle("Test Window");
-
+        
     }
 
     @Override
     public void process() {
-        
-        ImGui.text("This is come text");
+
+        ImGui.setNextWindowPos(25, 25);
+        ImGui.setNextWindowSize(100, 100);
+        ImGui.begin("test container", ImGuiWindowFlags.NoResize | 
+            ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoMove);
+        ImGui.checkbox("checked", checked);
+        ImGui.end();
 
     }
 }
